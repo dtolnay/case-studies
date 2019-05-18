807e0b390c87d6b3616d853c24035c4498e5ff59
David Tolnay <dtolnay@gmail.com>
2019-05-18 16:31:59 -0700

    Demo implementation of readonly-fields

---
+++ readonly-fields/src/main.rs
@@ -0,0 +1,51 @@
+pub use oqueue::Task;
+
+mod oqueue {
+    use core::ops::Deref;
+
+    #[derive(Default)]
+    #[repr(C)]
+    pub struct Task {
+        #[cfg(oqueue_docs)]
+        pub index: usize,
+
+        #[cfg(not(oqueue_docs))]
+        index: usize,
+
+        /* other private fields */
+        q: usize,
+    }
+
+    #[doc(hidden)]
+    #[repr(C)]
+    pub struct ReadOnlyTask {
+        pub index: usize,
+
+        /* the same private fields */
+        q: usize,
+    }
+
+    #[doc(hidden)]
+    impl Deref for Task {
+        type Target = ReadOnlyTask;
+
+        fn deref(&self) -> &Self::Target {
+            unsafe { &*(self as *const Self as *const Self::Target) }
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn from_within_module(task: &mut Task) {
+        task.index += 1;
+    }
+}
+
+fn from_outside_module(task: &mut Task) {
+    task.index += 1; // cannot assign
+}
+
+fn main() {
+    let mut task = Task::default();
+    oqueue::from_within_module(&mut task);
+    from_outside_module(&mut task);
+}
